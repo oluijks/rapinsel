2cc5b35fdf73a2609a8481a1576f542d89bf2a88
Olaf Luijks <olafluijks@proton.me>
2024-07-16 08:36:54 +0200

    chore: Removed some use statements

--- packages/backend/src/main.rs
+++ packages/backend/src/main.rs
@@ -6,36 +6,31 @@ mod model;
 mod scopes;
 mod util;
 
-use actix_cors::Cors;
-use actix_web::{http::header, middleware::Logger, web::scope, web::Data, App, HttpServer};
+use actix_web::{http::header, web::scope, App, HttpServer};
 use api::{health::health_check_handler, info::api_info_handler};
-use config::Config;
-use dotenv::dotenv;
-use scopes::faqs::scoped_faqs_config;
-use sqlx::postgres::PgPoolOptions;
-use sqlx::{Pool, Postgres};
-use std::error::Error;
 
 #[derive(Debug, Clone)]
 pub struct AppState {
-    db: Pool<Postgres>,
+    db: sqlx::Pool<sqlx::Postgres>,
 }
 
 const API_SCOPE: &str = "/api/";
 
 #[actix_web::main]
-async fn main() -> Result<(), Box<dyn Error>> {
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     if std::env::var_os("RUST_LOG").is_none() {
         std::env::set_var("RUST_LOG", "actix_web=info");
     }
 
-    dotenv().ok();
+    dotenv::dotenv().ok();
     env_logger::init();
 
-    let config: Config = Config::init();
+    let config: config::Config = config::Config::init();
 
     let scoped_api_version: String = API_SCOPE.to_owned() + &config.version;
-    let pool: Pool<Postgres> = match PgPoolOptions::new()
+
+    // region:  database connection pool
+    let pool: sqlx::Pool<sqlx::Postgres> = match sqlx::postgres::PgPoolOptions::new()
         .max_connections(10)
         .connect(&config.pg_conn_str)
         .await
@@ -49,14 +44,16 @@ async fn main() -> Result<(), Box<dyn Error>> {
             std::process::exit(1);
         }
     };
+    // endregion:  database connection pool
 
     println!(
         "{}",
-        format_args!("[api] server is running on {}:{}", config.host, config.port)
+        format_args!("[api] listening on {}:{}", config.host, config.port)
     );
 
     HttpServer::new(move || {
-        let cors = Cors::default()
+        // region:  CORS
+        let cors = actix_cors::Cors::default()
             .allowed_methods(vec!["GET", "POST", "PUT", "DELETE"])
             .allowed_headers(vec![
                 header::ACCEPT,
@@ -67,14 +64,15 @@ async fn main() -> Result<(), Box<dyn Error>> {
             .allowed_origin("https://rapinsel.io")
             .allowed_origin("http://localhost:5173")
             .supports_credentials();
+        // endregion:  CORS
 
         App::new()
             .wrap(cors)
-            .wrap(Logger::default())
-            .app_data(Data::new(AppState { db: pool.clone() }))
+            .wrap(actix_web::middleware::Logger::default())
+            .app_data(actix_web::web::Data::new(AppState { db: pool.clone() }))
             .service(
                 scope(&scoped_api_version.to_owned())
-                    .configure(scoped_faqs_config)
+                    .configure(scopes::faqs::scoped_faqs_config)
                     .service(api_info_handler)
                     .service(health_check_handler),
             )
